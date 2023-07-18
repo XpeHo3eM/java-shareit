d621d6358ac50d00a3793bbd8ec25ba4830eab03
Никулин Павел <XpeHo3eM@gmail.com>
2023-07-18 11:33:09 +0400

    добавлен хэндлер для internal_server_error

--- src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.exception.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,9 +29,16 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler({NotAvailableBookingException.class,
-            ValidationException.class})
+            ValidationException.class,
+            MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> badRequest(final RuntimeException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(final Throwable e) {
         return Map.of("error", e.getMessage());
     }
 }
